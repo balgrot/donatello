72a86e870f22a1174fd67020e3e3763296ffdc56
Jorge Fernandez <jorgefa.dev@gmail.com>
2024-11-17 12:45:11 -0600

    Revert "First Commit"
    This reverts commit e0717a4032348b0470fa2250d6720f7fd5d501f1.

--- orgs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from orgs.models import Organization
-from orgs.models import OauthToken
-# Register your models here.
-
-admin.site.register(Organization)
-admin.site.register(OauthToken)
